c1344c3ff9c1046564086f073e60e90707556293
Nikita Yerameey <smertskosoii@gmail.com>
2024-04-10 17:49:15 +0300

    Did sprint 1 week 2 video bofore 1:01:55

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './App.css';
 import { TaskType, Todolist } from './components/todolist/Todolist';
 
@@ -13,32 +14,42 @@ import { TaskType, Todolist } from './components/todolist/Todolist';
 
 function App() {
 
+    
+    const TodolistTitle = "Band's concert to visit";
+    
 
-    const tasks_1: Array<TaskType> = [
-        {id: 1, title: "HTML & CSS", isDone: true},
-        {id: 2, title: "JavaScript", isDone: false},
-        {id: 3, title: "React", isDone: false},
 
-    ]
-    const tasks_2: Array<TaskType> = [
-        {id: 1, title: "Metallica", isDone: true},
-        {id: 2, title: "Slayer", isDone: true},
-        {id: 3, title: "Nirvana", isDone: true},
-        {id: 4, title: "Type0Negative", isDone: true},
-    ];
 
-    const tasks_3: Array<TaskType> = [
-        {id: 1, title: "The catcher in the rye", isDone: true},
-        {id: 2, title: "American Gods", isDone: false},
-        {id: 3, title: "It", isDone: false},
-    ]
+    
+    
+
+        const [tasks, setTasks] = useState<Array<TaskType>>([
+            {id: 1, title: "Metallica", isDone: true},
+            {id: 2, title: "Slayer", isDone: true},
+            {id: 3, title: "Nirvana", isDone: true},
+            
+        ])
+       
+
+       const removeTask = (taskId: number) => {
+            //delited task frim array
+            //new Array
+            setTasks([
+            {id: 1, title: "Metallica", isDone: true},
+            {id: 2, title: "Slayer", isDone: true},
+            {id: 3, title: "Nirvana", isDone: true},
+            
+        ])
+
+       }
+   
 
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks_1} />
-            <Todolist title="Band's concert to visit" tasks={tasks_2} />
-            <Todolist title="Books to read" tasks={tasks_3} />
+            
+            <Todolist title= {TodolistTitle} tasks={tasks} />
+            
         </div>
     );
 }
