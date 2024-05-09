6e43b6a5468d97777e9c54cce604bae924efc64f
Nikita Yerameev <Smertskosoii@gmail.com>
2024-05-09 19:18:08 +0300

    Done all To-Do-List-3 sprint 1

--- src/components/todolist/Button.tsx
+++ src/components/todolist/Button.tsx
@@ -3,11 +3,12 @@
 type ButtonPropsType = {
   title: string
   onClickHandler?: () => void
+  disabled?: boolean
 }
 
-export const Button = ({title, onClickHandler}: ButtonPropsType) => {
+export const Button = ({title, onClickHandler, disabled}: ButtonPropsType) => {
     return(
-      <button onClick={onClickHandler}>
+      <button onClick={onClickHandler} disabled = {disabled}>
         <span>{title}</span>
       </button>
     )
--- src/components/todolist/TodolistWithRef.tsx
+++ src/components/todolist/TodolistWithRef.tsx
@@ -1,5 +1,5 @@
   import {FilterValuesType} from "../../App"
-  import React, { KeyboardEvent, useState } from 'react';
+  import React, { useRef, useState } from 'react';
 import { Button } from "./Button";
 
 type TodolistProopsTupe = {
@@ -23,10 +23,7 @@ export const Todolist = (props: TodolistProopsTupe) => {
 
   //const tasks_list = tasks.map()
 //Local State
-const [filter,setFilter] = useState<FilterValuesType>("all")
-const [taskTItle, setTaskTitle] = useState("") 
-
-console.log(taskTItle);
+const [filter,setFilter] = useState<FilterValuesType>("all") 
 
 
 
@@ -49,52 +46,38 @@ nextFilterValue: FilterValuesType) => {
 const tasksForTodoList = getTasksForTodoList(tasks, filter)
 
 
-
+const taskInputRef = useRef<HTMLInputElement>(null)
 const addTaskHandler = () => {
-    addTask(taskTItle)
-    setTaskTitle("")
+
+  if(taskInputRef.current) {
+    addTask(taskInputRef.current.value)
+    taskInputRef.current.value = ''
+ 
+  }
+ 
 }
 
-const addTaskOnKeyUpHandler = taskTItle.length === 0 
-? undefined 
-:(e : KeyboardEvent<HTMLInputElement>) => {
-  if(e.key === "Enter") {
-    addTaskHandler()
-  }
- }
 
   const taskList: Array<JSX.Element> = tasksForTodoList.map(tasks => {
     
     const removeTaskHandler = () => removeTask(tasks.id)
     
     return (
-      <li key={tasks.id}> 
+      <li>
           <input type="checkbox" checked={tasks.isDone}/> 
           <span>{tasks.title}</span>
           <button onClick={removeTaskHandler}>X</button>
       </li>
      )
     })
-
-
-
-  
   
 
   return (
     <div className="todolist">
     <h3>{title}</h3>
     <div>
-        <input value={taskTItle}
-               onChange={(e)=> setTaskTitle(e.currentTarget.value)}
-               onKeyUp={addTaskOnKeyUpHandler} 
-        />  
-
-        <Button title = "+"
-                onClickHandler={addTaskHandler}
-                disabled = {taskTItle.length === 0 || taskTItle.length > 30}/>
-
-                {taskTItle.length > 15 && <div>Too many characters</div>}
+        <input ref={taskInputRef}/>
+        <Button title = "+" onClickHandler={addTaskHandler}/>
         
     </div>
 
